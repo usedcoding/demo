2e96bce5b04eb5dae58bbc96979f7f56170a6c35
usedcoding <ja14759@gmail.com>
2023-11-24 12:41:50 +0900

    detail 400 error

--- src/main/java/com/example/demo/article/ArticleController.java
+++ src/main/java/com/example/demo/article/ArticleController.java
@@ -4,10 +4,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.Optional;
+
 @RequiredArgsConstructor
 @Controller
 public class ArticleController {
@@ -19,7 +22,9 @@ public class ArticleController {
     }
 
     @GetMapping("/article/list")
-    public String list() {
+    public String list(Model model) {
+        List<Article> articleList = this.articleService.getList();
+        model.addAttribute("articleList", articleList);
         return "article_list";
     }
 
@@ -34,9 +39,11 @@ public class ArticleController {
         return"redirect:/article/list";
     }
 
-    @GetMapping("/article/detail/{id}")
-    public String detail() {
-        return"";
+    @GetMapping(value = "/article/detail/{id}")
+    public String detail(Model model, @PathVariable Integer id) {
+        Article article = this.articleService.getArticle(id);
+        model.addAttribute("article", article);
+        return"article_detail";
     }
 
 }
--- src/main/java/com/example/demo/article/ArticleService.java
+++ src/main/java/com/example/demo/article/ArticleService.java
@@ -3,12 +3,24 @@ package com.example.demo.article;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
 public class ArticleService {
     private final ArticleRepository articleRepository;
+
+    public List<Article> getList() {
+       return this.articleRepository.findAll();
+    }
+
+    public Article getArticle(Integer id) {
+        Optional<Article> article = this.articleRepository.findById(id);
+        return article.get();
+    }
 
     public void create(String subject, String content) {
         Article article = new Article();
