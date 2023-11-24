7e745271d61c1749e29e8b30d4416d9393744330
usedcoding <ja14759@gmail.com>
2023-11-24 12:05:09 +0900

    detaile start

--- src/main/java/com/example/demo/article/ArticleController.java
+++ src/main/java/com/example/demo/article/ArticleController.java
@@ -1,11 +1,18 @@
 package com.example.demo.article;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+@RequiredArgsConstructor
 @Controller
 public class ArticleController {
+    private final ArticleService articleService;
+
     @GetMapping("/")
     public String root() {
         return "redirect:/article/list";
@@ -16,8 +23,20 @@ public class ArticleController {
         return "article_list";
     }
 
-    @GetMapping(value = "/article/create")
+    @GetMapping("/article/create")
     public String create() {
-        return"article_create";
+        return "article_create";
     }
+
+    @PostMapping(value = "/article/create")
+    public String create(@RequestParam String subject, @RequestParam String content) {
+        this.articleService.create(subject, content);
+        return"redirect:/article/list";
+    }
+
+    @GetMapping("/article/detail/{id}")
+    public String detail() {
+        return"";
+    }
+
 }
--- src/main/java/com/example/demo/article/ArticleService.java
+++ src/main/java/com/example/demo/article/ArticleService.java
@@ -1,7 +1,21 @@
 package com.example.demo.article;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
 @Service
 public class ArticleService {
+    private final ArticleRepository articleRepository;
+
+    public void create(String subject, String content) {
+        Article article = new Article();
+        article.setSubject(subject);
+        article.setContent(content);
+        article.setCreateDate(LocalDateTime.now());
+        this.articleRepository.save(article);
+
+    }
 }
