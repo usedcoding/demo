98caf8391bf98ed4b2d5aa60553956e6053b1f58
usedcoding <ja14759@gmail.com>
2023-11-24 11:17:56 +0900

    create

--- src/main/java/com/example/demo/article/ArticleController.java
+++ src/main/java/com/example/demo/article/ArticleController.java
@@ -2,6 +2,7 @@ package com.example.demo.article;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ArticleController {
@@ -15,7 +16,7 @@ public class ArticleController {
         return "article_list";
     }
 
-    @GetMapping("/article/create")
+    @GetMapping(value = "/article/create")
     public String create() {
         return"article_create";
     }
